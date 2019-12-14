5d2dbe6fb3e09db0063533349b7dab182c51ff58
windofnet <windofnet@hotmail.com>
2019-12-14 23:58:43 +0800

    src

---
+++ Main.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace CgStairFinder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.listBox1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel2, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.linkLabel2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 6);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 7;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(250, 300);
+            this.tableLayoutPanel2.TabIndex = 14;
+            // 
+            // listBox1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.listBox1, 2);
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(3, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 162);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox_DrawItem);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.flowLayoutPanel2, 2);
+            this.flowLayoutPanel2.Controls.Add(this.label3);
+            this.flowLayoutPanel2.Controls.Add(this.numericUpDown1);
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 252);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(244, 29);
+            this.flowLayoutPanel2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "頻率 (秒)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(64, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 23);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "開始偵測";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 49);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(244, 29);
+            this.flowLayoutPanel1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "刷新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(80, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "偵測視窗";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.linkLabel2, 2);
+            this.linkLabel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel2.Location = new System.Drawing.Point(3, 0);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(244, 16);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "資料夾路徑";
+            this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.SetColumnSpan(this.label2, 2);
+            this.label2.Location = new System.Drawing.Point(3, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 16);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.comboBox1, 2);
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(157, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "紀錄";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 261);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(250, 300);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CgStairFinder
+{
+    public partial class Main : Form
+    {
+        public class Log
+        {
+            public string MapCode { get; set; }
+            public string MapName { get; set; }
+            public List<CgStair> CgStairs { get; set; }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        [DllImport("kernel32.dll")]
+        private static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int nSize, int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        private static extern int OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll")]
+        private static extern void CloseHandle(int hObject);
+
+        static IDictionary<string, Log> logs = new Dictionary<string, Log>();
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            ShowCgDir();
+            CgListReload();
+        }
+
+        private void CgListReload()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("不選擇視窗");
+            comboBox1.Items.AddRange(Process.GetProcessesByName("bluecg"));
+            comboBox1.Items.AddRange(Process.GetProcessesByName("bluehd"));
+            comboBox1.Items.AddRange(Process.GetProcessesByName("cg"));
+            comboBox1.DisplayMember = "MainWindowTitle";
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void ShowCgDir()
+        {
+            var cgDir = Settings.Default.cgDir;
+
+            if (string.IsNullOrEmpty(cgDir))
+            {
+                this.linkLabel2.Text = "未設定魔力寶貝目錄";
+                return;
+            }
+            else
+            {
+                this.linkLabel2.Text = cgDir;
+            }
+
+            toolTip1.SetToolTip(this.linkLabel2, this.linkLabel2.Text);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog folderSelectionDialog = new FolderBrowserDialog();
+            folderSelectionDialog.Description = "選擇魔力寶貝資料夾";
+            folderSelectionDialog.ShowDialog();
+            Settings.Default.cgDir = folderSelectionDialog.SelectedPath;
+            Settings.Default.Save();
+            ShowCgDir();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.timer1.Enabled)
+            {
+                stop();
+                return;
+            }
+
+            var cgDir = Settings.Default.cgDir;
+            if (string.IsNullOrEmpty(cgDir) ||
+                !Directory.Exists($"{cgDir}\\map"))
+            {
+                MessageBox.Show(this, "啟動失敗, 請確認路徑是否正確", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            start();
+        }
+
+        private void start()
+        {
+            this.timer1.Interval = 10;
+            this.timer1.Start();
+            this.button1.Text = "停止偵測";
+            this.numericUpDown1.Enabled = false;
+            button2.Enabled = false;
+            comboBox1.Enabled = false;
+        }
+
+        private void stop()
+        {
+            this.timer1.Stop();
+            this.label2.ResetText();
+            this.listBox1.Items.Clear();
+            this.button1.Text = "開始偵測";
+            this.numericUpDown1.Enabled = true;
+            button2.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.timer1.Interval = (int)this.numericUpDown1.Value * 1000;
+
+            int? hProcess = null;
+            var mapName = string.Empty;
+
+            if (comboBox1.SelectedIndex > 0)
+            {
+                var p = (Process)comboBox1.SelectedItem;
+                if (p.HasExited)
+                {
+                    throw new Exception();
+                }
+
+                hProcess = OpenProcess(0x1F0FFF, false, p.Id);
+
+                if (hProcess.HasValue)
+                {
+                    var readMapNameBuffer = new byte[32];
+                    ReadProcessMemory(hProcess.Value, 0x95C870, readMapNameBuffer, readMapNameBuffer.Length, 0);
+                    mapName = Encoding.Default.GetString(readMapNameBuffer.TakeWhile(x => x != 0).ToArray());
+                }
+
+                if (!string.IsNullOrEmpty(mapName))
+                {
+                    this.Text = mapName;
+                }
+            }
+
+            try
+            {
+                var lastFile = new DirectoryInfo($"{Settings.Default.cgDir}\\map").GetFiles("*.dat", SearchOption.AllDirectories).OrderByDescending(f => f.LastWriteTime).FirstOrDefault();
+                if (lastFile is null)
+                {
+                    return;
+                }
+
+                if (lastFile.FullName.Length > 18)
+                {
+                    char[] c = lastFile.FullName.ToCharArray();
+                    Array.Reverse(c);
+                    Array.Resize(ref c, 16);
+                    Array.Reverse(c);
+                    this.label2.Text = $"..{new string(c)}";
+                }
+                else
+                {
+                    this.label2.Text = $"{lastFile.FullName}";
+                }
+
+                listBox1.Items.Clear();
+                var cgStairs = new CgMapStairFinder(lastFile).GetStairs();
+
+                if (cgStairs.Count == 0)
+                {
+                    listBox1.Items.Add("沒有找到任何樓梯");
+                }
+                else
+                {
+                    logs[lastFile.Name] = new Log { MapCode = lastFile.Name, MapName = mapName, CgStairs = cgStairs };
+
+                    foreach (var c in cgStairs)
+                    {
+                        string type = string.Empty;
+                        switch (c.Type)
+                        {
+                            case StairType.Down: type = "下樓"; break;
+                            case StairType.Up: type = "上樓"; break;
+                            case StairType.Jump: type = "可移動"; break;
+                            case StairType.Unknow: type = "不明"; break;
+                        }
+
+                        if (!hProcess.HasValue)
+                        {
+                            listBox1.Items.Add($"東{c.East}, 南{c.South} -- {type}");
+                        }
+                        else
+                        {
+                            int east = 0, south = 0;
+                            byte[] buffer = new byte[32];
+                            ReadProcessMemory(hProcess.Value, 0xBF6B54, buffer, 2, 0);
+                            east = BitConverter.ToInt16(buffer, 0);
+                            ReadProcessMemory(hProcess.Value, 0xBF6C1C, buffer, 2, 0);
+                            south = BitConverter.ToInt16(buffer, 0);
+
+                            string direction = string.Empty;
+
+                            var r = Math.Atan2(c.East - east, c.South - south) / Math.PI * 180;
+
+                            if (r <= -135 + 22.5 && r >= -135 - 22.5)
+                            {
+                                direction = "←";
+                            }
+                            if (r <= -90 + 22.5 && r >= -90 - 22.5)
+                            {
+                                direction = "↙";
+                            }
+                            if (r <= -45 + 22.5 && r >= -45 - 22.5)
+                            {
+                                direction = "↓";
+                            }
+                            if (r <= 0 + 22.5 && r >= 0 - 22.5)
+                            {
+                                direction = "↘";
+                            }
+                            if (r <= 45 + 22.5 && r >= 45 - 22.5)
+                            {
+                                direction = "→";
+                            }
+                            if (r <= 90 + 22.5 && r >= 90 - 22.5)
+                            {
+                                direction = "↗";
+                            }
+                            if (r <= 135 + 22.5 && r >= 135 - 22.5)
+                            {
+                                direction = "↑";
+                            }
+                            if (r < -135 - 22.5 || (r <= 180 + 22.5 && r >= 180 - 22.5))
+                            {
+                                direction = "↖";
+                            }
+
+                            listBox1.Items.Add($"東{c.East}, 南{c.South} {direction} -- {type}");
+                        }
+                    }
+                }
+            }
+            catch (IOException) { return; }
+            catch (Exception)
+            {
+                stop();
+                MessageBox.Show(this, "發生錯誤, 自動偵測已停止", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (hProcess.HasValue)
+                {
+                    CloseHandle(hProcess.Value);
+                }
+            }
+        }
+
+        private void listBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            if (e.Index == -1)
+            {
+                return;
+            }
+
+            Brush itemColor = Brushes.White;
+            if (((ListBox)sender).Items[e.Index].ToString().Contains("上樓"))
+            {
+                itemColor = Brushes.SpringGreen;
+            }
+            else if (((ListBox)sender).Items[e.Index].ToString().Contains("下樓"))
+            {
+                itemColor = Brushes.OrangeRed;
+            }
+            else if (((ListBox)sender).Items[e.Index].ToString().Contains("可移動"))
+            {
+                itemColor = Brushes.Silver;
+            }
+
+            e.Graphics.FillRectangle(itemColor, e.Bounds);
+
+            e.Graphics.DrawString(((ListBox)sender).Items[e.Index].ToString(), this.Font, Brushes.Black, e.Bounds);
+            e.DrawFocusRectangle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CgListReload();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process process = comboBox1.SelectedItem as Process;
+            if (process is null)
+            {
+                return;
+            }
+
+            ShowWindow(process.MainWindowHandle, 9);
+            SetForegroundWindow(process.MainWindowHandle);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            var tmpPath = Path.GetTempFileName();
+            foreach (var kv in logs.OrderBy(x => x.Value.MapName))
+            {
+                var text = $"{kv.Key}";
+                if (!string.IsNullOrEmpty(kv.Value.MapName)) { text += $"({kv.Value.MapName})"; }
+                text += ": ";
+
+                text += string.Join(" | ", kv.Value.CgStairs.OrderBy(x => x.Type).Select(x =>
+                {
+                    string type = string.Empty;
+                    switch (x.Type)
+                    {
+                        case StairType.Down: type = "下樓"; break;
+                        case StairType.Up: type = "上樓"; break;
+                        case StairType.Jump: type = "可移動"; break;
+                        case StairType.Unknow: type = "不明"; break;
+                    }
+
+                    return $"{x.East}, {x.South} -- {type}";
+                }));
+
+                File.AppendAllText(tmpPath, text + "\r\n");
+            }
+
+            Process.Start("notepad.exe", tmpPath);
+
+            //var f = new LogViewer();
+            //f.Logs = logs;
+            //f.Show(this);
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (logs.Count > 0)
+            {
+                e.Cancel = MessageBox.Show(this, "是否要結束程式？ (紀錄將會清除)", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.No;
+            }
+        }
+    }
+}
